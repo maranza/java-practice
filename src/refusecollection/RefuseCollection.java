869f8fa8dd73e69a1074bc55fc72a07a27e6b3dc
maranza <o.maranza@gmail.com>
2019-03-08 20:38:49 +0200

    implemented: calculateTotalsAndAverages, and getHighestWeight

--- src/refusecollection/RefuseCollection.java
+++ src/refusecollection/RefuseCollection.java
@@ -1,5 +1,6 @@
 package refusecollection;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,9 +16,36 @@ public class RefuseCollection {
     
     static Scanner console = new Scanner(System.in);
     
+    static int [] sumOfWeights(int[][] array)
+    {
+        int index = 0;
+        int temp[] = new int[array[index].length];
+        for (int i = 0; i < array[0].length; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < array.length; j++)
+            {
+                sum += array[j][i];
+            }
+            temp[index] = sum;
+//            System.out.println("Index is: " + index + " Sum is: " + sum);
+            index++;
+        }
+        return temp;
+    }
+    
+    
     public static void main(String[] args)
     {
-        Random rand = new Random(10);
+        
+        int[] one = {1,2,3};
+        int[] two = {2,4,6};
+        
+        one = two;
+        
+        System.out.println("testing: "+ Arrays.toString(one));
+        
+        Random rand = new Random(50);
         int maxSchools = 6;
         int regions = 4;
         int[] regionNumber = new int[regions];
@@ -56,36 +84,111 @@ public class RefuseCollection {
             for (int i = 0; i < numberOfSchoolsPerRegion; i++) //rows
             {
                 
-                for (int j = 0; j < regionNumber.length; j++) //columns
+                for (int j = 0; j < regions; j++) //columns
                 {
                     //System.out.println("enter weight of litter of school in " + j + " participated in Region " + (i+1));
                     //weightOfLitter = console.nextInt();
                     weightOfLitter = rand.nextInt(800);
                     rc[j][i] = weightOfLitter;
-                    rc[j][numberOfSchoolsPerRegion + 1] += weightOfLitter; // Not Storing (only store last value)
-                    System.out.print("test:" + rc[j][numberOfSchoolsPerRegion + 1] + "\t");
                 }
                 System.out.println("\n");
             }
             
-            System.out.println("===================================\n");
-            for (int i = 0; i < regionNumber.length; i++)
-            {
-                System.out.print(rc[i][numberOfSchoolsPerRegion + 1] + "\t");
-            }
+//            System.out.println("===================================\n");
+//            for (int i = 0; i < regionNumber.length; i++)
+//            {
+//                System.out.print(rc[i][numberOfSchoolsPerRegion + 1] + "\t");
+//            }
+
+
+            System.out.println("===========================Calculating sums===================================\n");
             
+            
+            
+//            System.out.println("sums : " + Arrays.toString(sumOfWeights(rc)));
+//            int columnSums[] = new int[regions];
+//            for (int i = 0; i < rc.length; i++)
+//            {
+//                for (int j = 0; j < rc[i].length; j++)
+//                {
+//                    columnSums[j] += rc[i][j];
+//                }
+//            }
+//            
+//            System.out.println("The sums of each column are: ");
+//            for (int i = 0; i < columnSums.length; i++)
+//            {
+//                System.out.print("Column " + i + ": " + columnSums[i] + "\t");
+//            }
+            
+            System.out.println("===========================Printing Array Values==============================\n");
             for (int i = 0; i < numberOfSchoolsPerRegion; i++)
             {
-                for (int j = 0; j < regionNumber.length; j++)
+                for (int j = 0; j < regions; j++)
                 {
-//                    System.out.print(rc[j][i] + "\t");
+                    System.out.print(rc[j][i] + "\t");
                 }
-//                System.out.println("\n");
+                System.out.println("\n");
             }
+            
+            System.out.println("================================================================================\n");
+            
+            
+            System.out.println("===========================Printing Array Values==============================\n");
+            int[][] as = calculateTotalsAndAverages(rc, regions);
+            for (int i = 0; i < as.length; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    System.out.print(as[i][j] + "\t");
+                }
+                System.out.println("\n");
+            }
+
+            System.out.println("================================================================================\n");
+            
+            System.out.println("sums and avg " + Arrays.toString(calculateTotalsAndAverages(rc, regions)));
+            System.out.println("highest weight: " + getHighestWeight(rc, maxSchools, regions));
+            
         }else{
             System.out.println("System Error: MAximum number of schools allowed per Region is 6");
         }
         
+    }
+    
+    static int[][] calculateTotalsAndAverages(int[][] data, int regions){
+        
+        int columnSum = 0;
+        int[][] sumArray = new int[2][4];
+        for (int k = 0; k < regions; k++)
+        {
+            for (int i = 0; i < data.length; i++)
+            {
+                columnSum += data[k][i];
+//                System.out.println("Column sum: " + columnSum);
+            }
+            sumArray[0][k] = columnSum;
+            sumArray[1][k] = columnSum/data.length;
+            columnSum = 0;
+        }
+//        System.out.println("sums : " + Arrays.toString(sumArray));
+        
+        return sumArray;
+    }
+    
+    static int getHighestWeight(int data[][], int maxSchools, int  maxRegion ){
+        int max = 0;
+        for (int i = 0; i < maxRegion; i++)
+        {
+            for (int j = 0; j < maxSchools; j++)
+            {
+                if (data[i][j] > max)
+                    max = data[i][j];
+            }
+            System.out.println("\n");
+        }
+
+        return max;
     }
     
 }
